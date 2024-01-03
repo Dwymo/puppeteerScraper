15f7f90abc8548317544a01a70e6fc077fb45e40
wymo <44616211+Dwymo@users.noreply.github.com>
2024-01-03 09:10:31 +0000

    Adjustments
    Moved the Investigative bloating to README file and removed some test code to make it easier to see what is being used presently and what wasn't needed

--- scrape.js
+++ scrape.js
@@ -15,8 +15,6 @@ const puppeteer = require('puppeteer');
     //Add a wait with a specified time to prevent early timeout if the page takes too long to load
     await new Promise((resolve) => setTimeout(resolve, 3000))
 
-
-
     // ASDA Scrape
     // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
@@ -42,48 +40,9 @@ const puppeteer = require('puppeteer');
      
     
     // Product allNutritionRows
-    /* 
-      XPath for the Nutrition information Panel appears to be different depending on Product, For example:
-      The product pages for 'Asda Baking Potatoes' and 'Warburtons English muffins' are slightly different (some products have more changes than others).
-      This causes the xpath for the nutritional tables to be different.
-
-      Asda-british-fluffy-golden-large-baking-potatoes
-      https://groceries.asda.com/product/baking-jacket-potatoes/asda-british-fluffy-golden-large-baking-potatoes/24932
-      *[@id="main-content"]/main/div[3]/div[2]/div[3]/div/div/div[2]/div[1]/div[2]/div/div[3]/div[2]
-
-      Warburtons-english-breakfast-muffins
-      https://groceries.asda.com/product/muffins-potato-cakes/warburtons-english-breakfast-muffins/910000203040
-      *[@id="main-content"]/main/div[3]/div[2]/div[3]/div/div/div[2]/div[4]/div[2]/div/div[3]/div[2]
-
-      Notice that 'div[1]' on Potatoes is 'div[4]' on muffins.
-
-      Each nutritional table does hower appear to have the following XPath (when using the specific class name):
-      //div[@class="pdp-description-reviews__nutrition-row pdp-description-reviews__nutrition-row--details"]
-      This has been tesetd on multiple items which appears to work so can be used to locate all of the Nutritional Data for each product.
-
-      The tables themselves also have slight changesm for example:
-      Asda Baking Potatoes say '(baked, flesh and skin) Per 100g' and the calories are just the number.
-      Warburtons English muffins say 'Per 100g of product and the calories have 'kcal' after the number.
-
-      Due to this, it might be benefitial to not take this information and instead, create a universal 100g/100ml per product as the base line.
-      Each table does however appear to have the same structure at least with each nutrional value being in the same order:
-
-      Energy
-      Calories
-      Fat
-      of with saturates
-      Carbohydrate
-      of which sugars
-      Fibre
-      Protein
-      Salt
-
-      Adding each value into an array and creating an object using the above structure should be accurate without the need to create an advanced way to match each value with the appropriate name
-    */
-
-      // Xpath using the class that seams to be universal for each nutritional row
+    // Xpath using the class that seams to be universal for each nutritional row: (Refer to README for more info)
     const allNutritionRowsxpath = '//div[@class="pdp-description-reviews__nutrition-row pdp-description-reviews__nutrition-row--details"]';
-      // All Required Values
+    // Getting all the Nutritional Values
     const allNutritionRowsvalue = await page.evaluate((allNutritionRowsxpath) => {
       // Looking for all Results that have the XPath Above (allNutritionRowsxpath)
       const element = document.evaluate(allNutritionRowsxpath, document, null, XPathResult.ANY_TYPE, null);
@@ -93,7 +52,6 @@ const puppeteer = require('puppeteer');
       let allElementsArray = [];
       // Create an empty object to input all of the Array data into once the array has been completed
       let AllElementsObject = {};
-
       // While the current element (result) is being evaluated, do the following.
       while (thisElement) {
         // Locate all child elements inside of the current element
@@ -119,7 +77,7 @@ const puppeteer = require('puppeteer');
         // Iterate to the next element in the list
         thisElement = element.iterateNext();
       }
-
+      // Once all the elements have been iterated through, Create an Object for all of the data, asigning each value the corresponding nutritional title 
       AllElementsObject = {
         Energy: allElementsArray[0],
         Calories: allElementsArray[1],
@@ -131,86 +89,9 @@ const puppeteer = require('puppeteer');
         Protein: allElementsArray[7],
         Salt: allElementsArray[8],
       }
-      
       // Once all Elements have been iterated through, return the array as the result for: allNutritionRowsvalue
       return AllElementsObject;
-
     }, allNutritionRowsxpath);
-
-
-
-
-    // ORDERED_NODE_ITERATOR_TYPE
-
-    //*[@id="productInformation"]/div[3]/div/div[1]/div[2]/div/div/div/div[1]
-    
-    /*
-    //Product allNutritionRows - Morrisons
-    const allNutritionRowsxpath = '//div[@class="bop-nutritionData__origin "]'; 
-    const allNutritionRowsvalue = await page.evaluate((allNutritionRowsxpath) => {
-      const element = document.evaluate(allNutritionRowsxpath, document, null, XPathResult.ANY_TYPE, null);
-      let thisElement = element.iterateNext();
-      // let allElements = 'List of All Nutritional Information: \n'
-      let allElements = 'List of per 100g Nutritional Information: \n'
-      // while (thisElement){
-      //   allElements += `${thisElement.textContent}\n`; 
-      //   thisElement = element.iterateNext();
-      // }
-
-      while (thisElement) {
-        const childElements = thisElement.querySelectorAll('*'); // Get all child elements
-        // for (const child of childElements) {
-        const specificChildIndex = 1; // Replace with the index of the specific child element you want to select
-        const specificChildElement = childElements[specificChildIndex];
-        if (specificChildElement) {
-          allElements += `${specificChildElement.textContent} `;
-        }
-        //allElements += ` ${child.textContent} ||`; // Add a space after each child element's textContent
-        // }
-        allElements += '\n'; // Add a newline after each main element
-        thisElement = element.iterateNext();
-      }
-  
-      console.log('All Nutritional Table ' + allElements);  
-      return allElements;
-
-    }, allNutritionRowsxpath);
-    */
-
-    /*
-    const tdElements = await page.$$eval('.bop-nutritionData__origin  tbody tr td', tds => {
-      let allData = '';
-    
-      tds.forEach(td => {
-        allData += `${td.textContent} `;
-      });
-    
-      console.log(allData.trim());
-      return allData.trim();
-    });
-*/
-    //Create an array of All the Nutrional Information from an entered URL:
-    function scrapeNutrition(request, response, next) {
-      allNutrition = [];
-      results.forEach(result => {
-        foodItem = {
-          id: result.id, // Not going to be shown to user by used in other functions
-          name: result.name,
-          measurement: result.amount + ' ' + result.measurement,
-          calories: result.calories,
-          // Will be visible in desktop mode to provide more info on larger screen but removed in mobile
-          carbohydrates: result.carbohydrates,
-          fat: result.fat,
-          protein: result.protein,
-          isPrivate: result.isPrivate
-        }
-        allFood.push(foodItem);
-        //console.log(allFood);
-      });
-      next();
-    }
-
-
 
     console.log(
       'Product Name:', productName,
@@ -218,8 +99,6 @@ const puppeteer = require('puppeteer');
       '\n',
       '\n','Nutritional Table, Per 100(g/ml):',
       '\n',allNutritionRowsvalue,
-      // '\nExtracted Entire Nutritional Table from tdElements: ', tdElements,
-
     );
   } catch (error) {
     console.error('Error occurred:', error);
